60104bf061d68769a7450420bacc26bdeffcd759
nahuelba <nahuelbarreiro@gmail.com>
2021-01-17 22:17:26 -0300

    profesionales hacer

--- viewpage/models.py
+++ viewpage/models.py
@@ -2,12 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class profesionales(models.Model):
-    
-
-    class Meta:
-        verbose_name_plural= "profesionales"
-
-    def __str__(self):
-        return self.titulo
 
